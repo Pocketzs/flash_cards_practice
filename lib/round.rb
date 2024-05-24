0fb34d1f419abef4a799b52a7051e13ce2137b73
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2024-05-24 14:19:39 -0500

    create round #start

--- lib/round.rb
+++ lib/round.rb
@@ -42,4 +42,25 @@ class Round
       turn.card_category == category
     end
   end
+
+  def start
+    system 'clear'
+    puts "Welcome! You're playing with #{@deck.card_count} cards."
+    puts "-------------------------------------------------"
+
+    @deck.cards.each_with_index do |card, index|
+      puts "This is card number #{index+1} out of #{@deck.card_count}."
+      puts "Question: #{card.question}"
+      input = gets.chomp
+      turn = take_turn(input)
+      puts turn.feedback
+      puts "-------------------------------------------------"
+    end
+
+    puts "****** Game over! ******"
+    puts "You had #{number_correct} correct guesses out of #{@turns.length} for a total score of #{percent_correct}%."
+    @deck.categories.each do |category|
+      puts "#{category} - #{percent_correct_by_category(category)}% correct"
+    end
+  end
 end
